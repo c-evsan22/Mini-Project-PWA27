1c3cecb12d229be8cd8c484ecffb834e58e6ab1a
Chris San <christianes322@gmail.com>
2023-11-28 13:19:59 -0600

    created outputs for webpack config and correct packasge json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,6 @@
+const { GenerateSW } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = () => {
@@ -11,16 +14,55 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Mini Project'
+      }),
+      
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Mini Project',
+        short_name: 'Mini Project',
+        description: 'Mini Project 11/28',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('client/src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
 
     }
   };
